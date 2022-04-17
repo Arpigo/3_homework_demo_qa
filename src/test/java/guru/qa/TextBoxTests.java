78aaffc8533038e2f5b15c6b977f446f414eec7e
egorm <emasorin@rvision.ru>
2022-04-17 21:11:28 +0300

    add test

--- src/test/java/guru/qa/TextBoxTests.java
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -1,14 +1,29 @@
 package guru.qa;
-
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests {
 
+    @BeforeAll
+    static void setUp(){
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
     @Test
     void fillFormTest(){
-        open("https://demoqa.com/automation-practice-form");
+    open("/text-box");
+
+    $("[id=userName]").setValue("Egor");
+    $("[id=userEmail]").setValue("egorm@mail.ru");
+    $("[id=currentAddress]").setValue("Russia");
+    $("[id=permanentAddress]").setValue("Velikij Novgorod");
+    $("[id=submit]").click();
+
     }
 
 }
