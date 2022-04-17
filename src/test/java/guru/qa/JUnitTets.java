9e379afa772e52b495ffe24517098f9295abb38c
egorm <emasorin@rvision.ru>
2022-04-17 19:54:30 +0300

    add slf4j

--- src/test/java/guru/qa/JUnitTets.java
+++ src/test/java/guru/qa/JUnitTets.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SimpleTest {
+public class JUnitTets {
 
     @BeforeEach
     void openYaPage(){
